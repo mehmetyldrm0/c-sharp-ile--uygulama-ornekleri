7cbc4fabc4ee15790ada56605bf7b5f5bde7902c
MEHMET YILDIRIM <mehmety.java@gmail.com>
2023-01-23 23:19:23 +0300

    Kombinasyon
    girilen sayıların kombinasyonunu bılma

--- kombinasyon alma .cs
+++ kombinasyon alma .cs
@@ -1,13 +1,4 @@
 
-            /* UYGULAMA SORULARI ÖRNEK8;
-              
-               BİLGİSAYARIN TUTTUĞU SAYIYI TAHMİN ETTİĞİMİZ BİR UYGULAMA GELİŞTİRİLİM 
-               BİLGİSAYAR 0-100 ARASI BİR SAYI TUTSUN. GİRİLEN TAHMİN RAKAIMINA GÖRE "DAHA BÜYÜK BİR SAYI GİRİNİZ "
-               YADA "DAHA KÜÇÜK BİR SAYI GİRİNİZ " DİYEREK YÖNLENDİRME YAPARAK GERÇEK SAYIYI BULMAMIZI SAĞLASIN
-               VE SONUCU DOĞRU BULDUĞUMUZDA KAÇ DENENEDEN SONRA DOĞRU BULĞUMUZADA GÖSTEREN UYGULAMAYI GELİŞTİRİN
-           
-             */
-
 using System;
 using System.Linq;
 
@@ -16,37 +7,42 @@ namespace ConsoleApp2
     {
         public static void Main()
         {
+            Console.WriteLine("birinci sayıyı giriniz");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("ikinci sayıyı giriniz");
+            int r = int.Parse(Console.ReadLine());
 
-
-            int number = new Random().Next(0, 100);
-            int count = 0;
-            int guess;
-            Console.WriteLine("BİLGİSAYAR 0 İLE 100 ARASINDA BİR SAYI TUTTU");
-
-            do
-            {
-                Console.Write("TAHMİNİNİZ : ");
-                guess = int.Parse(Console.ReadLine());
-
-                if (guess < number)
-                {
-                    Console.WriteLine("DAHA BÜYÜK BİR SAYI GİRİNİZ");
-                    count++;
-                }
-                else if (guess > number)
-                {
-                    Console.WriteLine("DAHA KÜÇÜK BİR SAYI GİRİNİZ");
-                    count++;
-                }
-
-                else
-                    Console.WriteLine("TEBRİKLER {0} DENEMEDEN SONRA DOĞRU BİLDİNİZ", count);
-
-            } while (guess != number);
+            Console.WriteLine("{0} sayısının {1} 'li  kombinasyonu :  {2}", n, r, combination(n, r));
 
 
         }
+
+        public static int combination(int n, int r)
+        {
+            int result = 1;
+
+            for (int i = 0; i < r; i++)
+            {
+                result *= n;
+                n--;
+
+            }
+            return result / Faktoryel(r);
+
+
+        }
+
+        public static int Faktoryel(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Faktoryel(n - 1);
+        }
+
+
+
     }
+}
 
    
 }
